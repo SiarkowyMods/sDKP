4fcef3fa92ae7df8b2b6eaac9055a46e32bd432f
Siarkowy <siarkowy6@gmail.com>
2013-02-13 12:35:48 +0100

    Changed TABs to spaces in Backup module.

--- sDKP_Backups/SlashCommand.lua
+++ sDKP_Backups/SlashCommand.lua
@@ -1,63 +1,63 @@
 --------------------------------------------------------------------------------
---	sDKP Backups (c) 2012 by Siarkowy
---	Released under the terms of GNU GPL v3 license.
+--  sDKP Backups (c) 2012 by Siarkowy
+--  Released under the terms of GNU GPL v3 license.
 --------------------------------------------------------------------------------
 
 local sDKP = sDKP
 local Util = sDKP.Util
 
 sDKP.Slash.args.backup = {
-	name = "Backup",
-	desc = "Backup management commands.",
-	type = "group",
-	args = {
-		create = {
-			name = "Create",
-			desc = "Saves new backup.",
-			type = "execute",
-			func = function(self, param)
-				local id = self:BackupNotes()
-				self:Print(
-					id	and	format("Guild <%s> notes' backup complete (%s).", self.guild, date("%d.%m.%Y %X", id))
-						or	"Could not backup officer notes. You have to be in a guild and have the permission to view officer notes for this to work."
-				)
-			end
-		},
-		delete = {
-			name = "Delete",
-			desc = "Deletes specified backup.",
-			type = "execute",
-			usage = "<timestamp>",
-			func = function(self, param)
-				if self:DeleteBackup(Util.ParamToTimestamp(param)) then
-					self:Print("Backup deleted.")
-				end
-			end
-		},
-		diff = {
-			name = "Diff",
-			desc = "Show differences between saved and current roster DKP values.",
-			type = "execute",
-			usage = "<timestamp>",
-			func = function(self, param)
-				self:VisualDiff(Util.ParamToTimestamp(param))
-			end
-		},
-		list = {
-			name = "List",
-			desc = "Lists all saved backups.",
-			type = "execute",
-			usage = "[<guild>]",
-			func = "BackupsList"
-		},
-		restore = {
-			name = "Restore",
-			desc = "Load saved backup to guild notes.",
-			type = "execute",
-			usage = "<timestamp>",
-			func = function(self, param)
-				self:Printf("%d |4note:notes; restored.", self:RestoreNotes(Util.ParamToTimestamp(param)) or 0)
-			end
-		}
-	}
+    name = "Backup",
+    desc = "Backup management commands.",
+    type = "group",
+    args = {
+        create = {
+            name = "Create",
+            desc = "Saves new backup.",
+            type = "execute",
+            func = function(self, param)
+                local id = self:BackupNotes()
+                self:Print(
+                    id  and format("Guild <%s> notes' backup complete (%s).", self.guild, date("%d.%m.%Y %X", id))
+                        or  "Could not backup officer notes. You have to be in a guild and have the permission to view officer notes for this to work."
+                )
+            end
+        },
+        delete = {
+            name = "Delete",
+            desc = "Deletes specified backup.",
+            type = "execute",
+            usage = "<timestamp>",
+            func = function(self, param)
+                if self:DeleteBackup(Util.ParamToTimestamp(param)) then
+                    self:Print("Backup deleted.")
+                end
+            end
+        },
+        diff = {
+            name = "Diff",
+            desc = "Show differences between saved and current roster DKP values.",
+            type = "execute",
+            usage = "<timestamp>",
+            func = function(self, param)
+                self:VisualDiff(Util.ParamToTimestamp(param))
+            end
+        },
+        list = {
+            name = "List",
+            desc = "Lists all saved backups.",
+            type = "execute",
+            usage = "[<guild>]",
+            func = "BackupsList"
+        },
+        restore = {
+            name = "Restore",
+            desc = "Load saved backup to guild notes.",
+            type = "execute",
+            usage = "<timestamp>",
+            func = function(self, param)
+                self:Printf("%d |4note:notes; restored.", self:RestoreNotes(Util.ParamToTimestamp(param)) or 0)
+            end
+        }
+    }
 }
