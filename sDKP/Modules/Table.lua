bb6eeedc65e0f4a0a5ab22f8159a9b756ce50e7b
Siarkowy <siarkowy6@gmail.com>
2013-09-19 18:57:27 +0200

    Table management module for future use implemented

---
+++ sDKP/Modules/Table.lua
@@ -0,0 +1,38 @@
+--------------------------------------------------------------------------------
+--  sDKP (c) 2011-2013 by Siarkowy
+--  Released under the terms of GNU GPL v3 license.
+--------------------------------------------------------------------------------
+
+local pairs = pairs
+local tinsert = tinsert
+local tremove = tremove
+local type = type
+
+-- Table pool management -------------------------------------------------------
+
+local pool = {}
+local dispose, table, wipe
+
+function table()
+    return tremove(pool) or {}
+end
+
+function wipe(t)
+    for k, v in pairs(t) do
+        if type(v) == "table" then dispose(t[k]) end
+        t[k] = nil
+    end
+
+    return t
+end
+
+function dispose(t)
+    if type(t) ~= "table" then return end
+    tinsert(pool, wipe(t))
+end
+
+-- Expose functions ------------------------------------------------------------
+
+sDKP.dispose = dispose
+sDKP.table = table
+sDKP.wipe = wipe
